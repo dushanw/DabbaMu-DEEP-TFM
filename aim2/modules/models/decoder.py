de33cafa239f9742489cb1c0603f32b349b1835d
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2021-06-10 23:11:59 -0400

    Added flexible decoder with variable layer numbers

--- aim2/modules/models/decoder.py
+++ aim2/modules/models/decoder.py
@@ -16,3 +16,38 @@ class simple_generator(nn.Module):
     def forward(self, x):
         x= x.view(-1, self.T, self.img_size, self.img_size)
         return self.model(x).view(-1, 1, self.img_size, self.img_size)
+    
+    
+class conv_relu_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding, stride):
+        super(conv_relu_block, self).__init__()
+        self.seq= nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size= kernel_size, padding=padding, stride=stride),
+                nn.ReLU())
+    def forward(self, x):
+        return self.seq(x)
+    
+    
+class genv1(nn.Module):
+    def __init__(self, T, img_size=32, img_channels=1, channel_list=[4,3,2,1]):
+        super(genv1, self).__init__()
+        self.T= T
+        self.img_size= img_size
+        self.img_channels=img_channels
+        self.channel_list= channel_list
+        
+        self.convrelu_blocks: nn.ModuleList[conv_relu_block] = nn.ModuleList()
+            
+        
+        self.convrelu_blocks.append(conv_relu_block(self.T, self.channel_list[0], kernel_size= 3, stride= 1, padding=1))
+        for idx in range(1, len(self.channel_list)):
+            self.convrelu_blocks.append(conv_relu_block(self.channel_list[idx-1], self.channel_list[idx], kernel_size= 3, stride= 1, padding=1))
+        self.convrelu_blocks.append(conv_relu_block(self.channel_list[-1], self.img_channels, kernel_size= 3, stride= 1, padding=1))
+    
+    def forward(self, x):
+        x= x.view(-1, self.T, self.img_size, self.img_size)
+        
+        for i in range(len(self.convrelu_blocks)):
+            x= self.convrelu_blocks[i](x)
+        
+        return x.view(-1, self.img_channels, self.img_size, self.img_size)
