e9ba33f8bb9cb63f08686a6c1056da198b82ba72
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2021-07-09 18:47:16 -0400

    Added impulse kernel used for sPSF, exPSF

--- aim2/modules/kernels.py
+++ aim2/modules/kernels.py
@@ -7,3 +7,10 @@ def get_gaussian(side_len=5, s=1):
     probs = [exp(-z*z/(2*s*s))/sqrt(2*pi*s*s) for z in range(-k,k+1)] 
     kernel = np.outer(probs, probs)
     return kernel
+
+
+def impulse(side_len=5):
+    mid = side_len//2
+    kernel = np.zeros((side_len,side_len), dtype= 'float')
+    kernel[mid, mid] = 1.0
+    return kernel
