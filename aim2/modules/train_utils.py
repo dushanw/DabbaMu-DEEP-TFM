Corrected validation metric calculation (ssim, mse): calculated on entire val set at last epoch

aim2/modules/train_utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 
 
+from modules.eval_metrics import ssim_ignite, mse_distance
 from modules.models.classifiers import classification_accuracy as accuracy
 
 def evaluate(device, loader, model_decoder, model_A, model_H, classifier, rescale=[-1, 1]):
@@ -30,8 +31,12 @@ def evaluate(device, loader, model_decoder, model_A, model_H, classifier, rescal
     return np.mean(acc_on_real), np.mean(acc_on_fake)
 
 
-def loop(device, loader, model_decoder, model_A, model_H, criterion, opt, type_= 'train', losses = [], epoch= None, m=1, train_model_iter=1, train_H_iter=0):
+def loop(device, loader, model_decoder, model_A, model_H, criterion, opt, type_= 'train', losses = [], epoch= None, m=1, train_model_iter=1, train_H_iter=0, metrics = None):
     losses_temp = []
+    metric_ssim11_temp = []
+    metric_ssim5_temp = []
+    metric_mse_temp = []
+    
     opt_model, opt_H = opt
     for i, (x, y) in enumerate(loader):
         #if i==10:break
@@ -72,9 +77,20 @@ def loop(device, loader, model_decoder, model_A, model_H, criterion, opt, type_=
                 X_hat = model_decoder(yt)
                 loss = criterion(X_hat, X)
         losses_temp.append(loss.item())
+        
+        metric_ssim11_temp.append(ssim_ignite(X_hat, X, k=11))
+        metric_ssim5_temp.append(ssim_ignite(X_hat, X, k=5))
+        metric_mse_temp.append(mse_distance(X_hat, X))
+        
+    
     print(f'yt range ({type_}): [{yt.min()} {yt.max()}]')
     losses.append(np.mean(losses_temp))
-    return losses, model_decoder, opt, X, X_hat, Ht, yt, model_H
+    
+    metrics['ssim11'].append(np.mean(metric_ssim11_temp))
+    metrics['ssim5'].append(np.mean(metric_ssim5_temp))
+    metrics['mse'].append(np.mean(metric_mse_temp))
+    
+    return losses, model_decoder, opt, X, X_hat, Ht, yt, model_H, metrics
 
 def train(model_decoder, model_A, model_H, criterion, opt, train_loader, test_loader, device, epochs=100, show_results_epoch=1, train_model_iter=1, train_H_iter=0, m_inc_proc= None, save_dir= None, classifier=None, rescale_for_classifier= [-1, 1], save_special_bool=False):
     
@@ -85,7 +101,10 @@ def train(model_decoder, model_A, model_H, criterion, opt, train_loader, test_lo
             return m
 
     losses_train= []
-    losses_test= []
+    losses_val= []
+    
+    metrics = {'mse':[], 'ssim5':[], 'ssim11':[]}
+    metrics_val = {'mse':[], 'ssim5':[], 'ssim11':[]}
     print(f'device : {device}')
 
     m=1
@@ -94,10 +113,10 @@ def train(model_decoder, model_A, model_H, criterion, opt, train_loader, test_lo
         print(f'm : {m}')
         
         start = time.time()
-        losses_train, model_decoder, opt, X, X_hat, Ht, yt, model_H = loop(device, train_loader, model_decoder, model_A, model_H, criterion, opt, 'train', losses_train, epoch, m, train_model_iter, train_H_iter)
+        losses_train, model_decoder, opt, X, X_hat, Ht, yt, model_H, metrics = loop(device, train_loader, model_decoder, model_A, model_H, criterion, opt, 'train', losses_train, epoch, m, train_model_iter, train_H_iter, metrics)
         end= time.time()
         
-        losses_test, model_decoder, opt, X_val, X_hat_val, Ht_val, yt_val, model_H = loop(device, test_loader, model_decoder, model_A, model_H, criterion, opt, 'test', losses_test, epoch, m, train_model_iter, train_H_iter)
+        losses_val, model_decoder, opt, X_val, X_hat_val, Ht_val, yt_val, model_H, metrics_val = loop(device, test_loader, model_decoder, model_A, model_H, criterion, opt, 'test', losses_val, epoch, m, train_model_iter, train_H_iter, metrics_val)
         
         
         print(f'training loop time (for single epoch): {end-start} sec')
@@ -107,7 +126,7 @@ def train(model_decoder, model_A, model_H, criterion, opt, train_loader, test_lo
         if epoch%show_results_epoch==0:
             if classifier==None:
                 class_acc_on_real, class_acc_on_fake=None, None
-            show_imgs(X_val, Ht_val, X_hat_val, yt_val, losses_train, losses_test, T, epoch, class_acc_on_real, class_acc_on_fake, save_dir, m) 
+            show_imgs(X_val, Ht_val, X_hat_val, yt_val, losses_train, losses_val, metrics_val, T, epoch, class_acc_on_real, class_acc_on_fake, save_dir, m) 
             
             if save_special_bool==True:
                 save_special_dir= f'{save_dir}/save_special'

aim2/modules/utils.py
@@ -57,7 +57,7 @@ def concat_imgs(save_dir, epoch, class_acc_on_fake=None, L1loss=None, metric_mse
     for img in glob.glob(f'{save_dir}/{epoch}_line*'):
         os.remove(img)
 
-def show_imgs(X, Ht, X_hat, yt, losses_train, losses_val,T, epoch, class_acc_on_real=None, class_acc_on_fake=None, save_dir=None, m=1):
+def show_imgs(X, Ht, X_hat, yt, losses_train, losses_val, metrics_val, T, epoch, class_acc_on_real=None, class_acc_on_fake=None, save_dir=None, m=1):
     #vmin=0
     #vmax=1
     
@@ -68,13 +68,14 @@ def show_imgs(X, Ht, X_hat, yt, losses_train, losses_val,T, epoch, class_acc_on_
         X = (X-X.min())/(X.max() - X.min()) 
         X_hat = torch.clamp((X_hat-X_hat.min())/(X_hat.max() - X_hat.min()), 0, 1)
         print(f'after normalization: show images : X range : [{X.min()}, {X.max()}] | X_hat range : [{X_hat.min()}, {X_hat.max()}]')
+
+        
+    metric_ssim11 = np.round(metrics_val['ssim11'][-1], 7)
+    metric_ssim5 = np.round(metrics_val['ssim5'][-1], 7)
+    metric_ssim= {'11':metric_ssim11, '5':metric_ssim5}
     
-    metric_ssim1 = np.round(ssim_ignite(X_hat, X, k=11), 7)
-    metric_ssim2 = np.round(ssim_ignite(X_hat, X, k=5), 7)
-    metric_ssim= {'11':metric_ssim1, '5':metric_ssim2}
-    
-    metric_mse = np.round( mse_distance(X_hat, X), 7)
-    
+    metric_mse = np.round(metrics_val['mse'][-1], 7)
+        
     if T>5:T=5
     if class_acc_on_fake==None or class_acc_on_real==None:class_acc_on_fake, class_acc_on_real = 'NA', 'NA'
     if save_dir!=None: