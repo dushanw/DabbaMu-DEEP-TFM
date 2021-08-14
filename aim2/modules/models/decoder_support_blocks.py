5128409a67b8d7be70cd3819b3e121c793bb0f28
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2021-08-14 09:27:25 -0400

    Added upsampling_custom1 helping block

--- aim2/modules/models/decoder_support_blocks.py
+++ aim2/modules/models/decoder_support_blocks.py
@@ -48,3 +48,32 @@ class ResNet_block_v1(nn.Module):
         out = self.conv_block(res_out)
         return out #no activation 
     
+    
+class upsample_custom1_block(nn.Module):
+    """
+    used in decoder_upsampling_nets.custom1()
+    
+    Idea: each spatial location with c-channel values will be upsampled to corresponding region of upsampled image through fully 
+    connected layers. Throughout the upsampling, number of channels will remain at T. 
+    Therefore one pixel (with all channels) of yt will be inputted to linear layer which have inputs of 
+    :: in_channels: T, out_channels: (upscale_factor**2)*T.
+    There are (yt_size*yt_size) pixels in yt. Therefore, there will be (yt_size*yt_size) number of linear layers.
+    """
+    def __init__(self, in_channels, out_channels, upscale_factor): #eg: (batch_size, in_channels)-> (batch_size, out_channels, upscale_factor, upscale_factor)
+        super(upsample_custom1_block, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.upscale_factor = upscale_factor
+
+        self.block = nn.Sequential(
+            nn.Linear(self.in_channels, (self.upscale_factor**2)*self.out_channels),
+            nn.ReLU()
+        )
+    def forward(self, x): # shape: (batch_size, in_channels) -> channel along single spatial location
+        x= x.view(-1, self.in_channels)
+        x= self.block(x)
+
+        x = x.view(-1, self.out_channels, self.upscale_factor, self.upscale_factor)
+
+        return x
