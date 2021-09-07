bd40d59c16e372985ab13bd89acdd0eec8ae0691
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2021-09-07 05:25:44 -0400

    Changed functionality to obtain heatmaps for single lr_H as well

--- aim2/modules/summarizing_utils/heatmaps.py
+++ aim2/modules/summarizing_utils/heatmaps.py
@@ -37,15 +37,21 @@ def create_metric_map(img_list, dict_, metric_name='SSIM', interested_key1= 'T',
     for key_ in override_dict:
         dict_[key_] = override_dict[key_]
     
+    if show_interested_img_names:print('interested attr dict : ', dict_)
+        
     interested_imgs = filter_results(img_list, dict_)
     
     if show_interested_img_names:print('interested_imgs : ', '\n'.join(interested_imgs))
     
-    #ax1_labels = sorted(dict_[interested_key1], key =float)
-    #ax2_labels = sorted(dict_[interested_key2], key =float)
-
-    ax1_labels = dict_[interested_key1]
-    ax2_labels = dict_[interested_key2]
+    try:
+        ax1_labels = sorted(dict_[interested_key1], key =float)
+    except:
+        ax1_labels = dict_[interested_key1]
+        
+    try:
+        ax2_labels = sorted(dict_[interested_key2], key =float)
+    except:
+        ax2_labels = dict_[interested_key2]
     
     metric_map = np.ones((len(ax1_labels),len(ax2_labels)), dtype='float')
     
@@ -60,7 +66,7 @@ def create_metric_map(img_list, dict_, metric_name='SSIM', interested_key1= 'T',
             for img_dir in interested_imgs:
                 if f'{interested_key1}({ax1_vals})' in img_dir and f'{interested_key2}({ax2_vals})' in img_dir:valid_img_dirs.append(img_dir)
 
-            assert len(valid_img_dirs)==1, "Multiple directories found for same attr combination (multiple values for one entry of heatmap). Check and add entry to 'override_dict' !!!"
+            assert len(valid_img_dirs)==1, f"Multiple directories found for same attr combination (multiple values for one entry of heatmap). Check and add entry to 'override_dict'/ No directories found at all !!! -> dirs :: {valid_img_dirs}"
 
             sorted_valid_img_dirs= sorted(valid_img_dirs, key= imgdir2metric)            
             selected_img_dir = sorted_valid_img_dirs[0]
@@ -70,46 +76,80 @@ def create_metric_map(img_list, dict_, metric_name='SSIM', interested_key1= 'T',
                 
     return metric_map, ax1_labels, ax2_labels
 
-def plot_heatmap(metric_map_highlrH, metric_map_lowlrH, vmin, vmax, x_ticks, y_ticks, interested_key1, interested_key2, override_dict, metric_name, save_dir):
-    plt.figure(figsize= (15,3))
-    plt.subplot(1,2,1)
-    ax = sns.heatmap(metric_map_highlrH, linewidth=0.5, annot=True, vmin=vmin, vmax= vmax, fmt= '.5f')
-    plt.xticks(np.arange(len(y_ticks))+0.5, y_ticks, rotation=0)
-    plt.xlabel(interested_key2)
-    plt.yticks(np.arange(len(x_ticks))+0.5, x_ticks, rotation=0)
-    plt.ylabel(interested_key1)
-    plt.title('Ht is learnable')
+def plot_heatmap(metric_map_highlrH, metric_map_lowlrH, highlrH, lowlrH, vmin, vmax, x_ticks, y_ticks, interested_key1, interested_key2, override_dict, metric_name, save_dir):
     
-    plt.subplot(1,2,2)
-    ax = sns.heatmap(metric_map_lowlrH, linewidth=0.5, annot=True, vmin=vmin, vmax= vmax, fmt= '.5f')
-    plt.xticks(np.arange(len(y_ticks))+0.5, y_ticks, rotation=0)
-    plt.xlabel(interested_key2)
-    plt.yticks(np.arange(len(x_ticks))+0.5, x_ticks, rotation=0)
-    plt.ylabel(interested_key1)
-    plt.title('Ht is fixed')
-    
-    suptit = f'{metric_name} -- '
-    overrides= ''
-    for key_, val_ in override_dict.items():
-        suptit+=f'{key_} : {val_} | '
-        overrides += f'{key_}({val_})@'
-    
-    suptit = suptit[:-3] 
-    overrides= overrides[:-1]
-    
-    plt.suptitle(suptit, y= 1.1)
-    
-    if save_dir!=None:
-        save_dir = f'{save_dir}/heatmaps'
+    if highlrH!= None and lowlrH!=None:
+        plt.figure(figsize= (15,3))
+        plt.subplot(1,2,1)
+        ax = sns.heatmap(metric_map_highlrH, linewidth=0.5, annot=True, vmin=vmin, vmax= vmax, fmt= '.5f')
+        plt.xticks(np.arange(len(y_ticks))+0.5, y_ticks, rotation=0)
+        plt.xlabel(interested_key2)
+        plt.yticks(np.arange(len(x_ticks))+0.5, x_ticks, rotation=0)
+        plt.ylabel(interested_key1)
+        plt.title(f'lr(Ht) : {highlrH}')
+
+        plt.subplot(1,2,2)
+        ax = sns.heatmap(metric_map_lowlrH, linewidth=0.5, annot=True, vmin=vmin, vmax= vmax, fmt= '.5f')
+        plt.xticks(np.arange(len(y_ticks))+0.5, y_ticks, rotation=0)
+        plt.xlabel(interested_key2)
+        plt.yticks(np.arange(len(x_ticks))+0.5, x_ticks, rotation=0)
+        plt.ylabel(interested_key1)
+        plt.title(f'lr(Ht) : {lowlrH}')
+
+        suptit = f'{metric_name} -- '
+        overrides= ''
+        for key_, val_ in override_dict.items():
+            suptit+=f'{key_} : {val_} | '
+            overrides += f'{key_}({val_})@'
+
+        suptit = suptit[:-3] 
+        overrides= overrides[:-1]
+
+        plt.suptitle(suptit, y= 1.1)
+
+        if save_dir!=None:
+            save_dir = f'{save_dir}/heatmaps'
+
+            try:os.mkdir(save_dir)
+            except:pass
+
+            np.save(f'{save_dir}/{metric_name}@@{overrides}@@highlrH.npy', metric_map_highlrH)
+            np.save(f'{save_dir}/{metric_name}@@{overrides}@@lowlrH.npy', metric_map_lowlrH)
+
+            plt.savefig(f'{save_dir}/{metric_name}@@{overrides}.jpg', bbox_inches='tight')
+        plt.show()
+    else:
         
-        try:os.mkdir(save_dir)
-        except:pass
+        assert (metric_map_highlrH== metric_map_lowlrH).all(), 'lr_H not specified and different lr_Hs are found !!!'
         
-        np.save(f'{save_dir}/{metric_name}@@{overrides}@@highlrH.npy', metric_map_highlrH)
-        np.save(f'{save_dir}/{metric_name}@@{overrides}@@lowlrH.npy', metric_map_lowlrH)
-        
-        plt.savefig(f'{save_dir}/{metric_name}@@{overrides}.jpg', bbox_inches='tight')
-    plt.show()
+        plt.figure(figsize= (8,3))
+        ax = sns.heatmap(metric_map_highlrH, linewidth=0.5, annot=True, vmin=vmin, vmax= vmax, fmt= '.5f')
+        plt.xticks(np.arange(len(y_ticks))+0.5, y_ticks, rotation=0)
+        plt.xlabel(interested_key2)
+        plt.yticks(np.arange(len(x_ticks))+0.5, x_ticks, rotation=0)
+        plt.ylabel(interested_key1)
+
+        suptit = f'{metric_name} -- '
+        overrides= ''
+        for key_, val_ in override_dict.items():
+            suptit+=f'{key_} : {val_} | '
+            overrides += f'{key_}({val_})@'
+
+        suptit = suptit[:-3] 
+        overrides= overrides[:-1]
+
+        plt.title(suptit, y= 1.1)
+
+        if save_dir!=None:
+            save_dir = f'{save_dir}/heatmaps'
+
+            try:os.mkdir(save_dir)
+            except:pass
+
+            np.save(f'{save_dir}/{metric_name}@@{overrides}.npy', metric_map_highlrH)
+
+            plt.savefig(f'{save_dir}/{metric_name}@@{overrides}.jpg', bbox_inches='tight')
+        plt.show()
     
     
 def plot_all_heat_maps(img_list, attr_dict_highlrH, attr_dict_lowlrH, interested_key1, interested_key2, override_dict, metric_name= 'SSIM', save_dir =None, show_interested_img_names= False):
@@ -123,21 +163,31 @@ def plot_all_heat_maps(img_list, attr_dict_highlrH, attr_dict_lowlrH, interested
     vmin = min(map_highlrH.min(), map_lowlrH.min())
     vmax =  max(map_highlrH.max(), map_lowlrH.max())
 
-    plot_heatmap(map_highlrH, map_lowlrH, vmin, vmax, xticks_highlrH, yticks_highlrH, interested_key1, interested_key2, override_dict, metric_name, save_dir)
+    if 'lr_H' in attr_dict_highlrH and 'lr_H' in attr_dict_lowlrH :
+        highlrH = attr_dict_highlrH['lr_H']
+        lowlrH= attr_dict_lowlrH['lr_H']
+    else:
+        highlrH, lowlrH= None, None
+    
+    plot_heatmap(map_highlrH, map_lowlrH, highlrH, lowlrH, vmin, vmax, xticks_highlrH, yticks_highlrH, interested_key1, interested_key2, override_dict, metric_name, save_dir)
     
     
     
-def quantitative_results_HEATMAPS(img_list, plot_vars_dict, overrides_dict_list, mode, save_dir, show_interested_img_names= False):
+def quantitative_results_HEATMAPS(img_list, plot_vars_dict, overrides_dict_list, mode, save_dir, show_interested_img_names= False, low_lr_H= '0.0', high_lr_H= '1.0'):
     plot_axes_variables= list(plot_vars_dict.keys())
     
     attr_dict_lowlrH = plot_vars_dict.copy()
     attr_dict_highlrH = plot_vars_dict.copy()
     
-    attr_dict_lowlrH['lr_H']= ['0.0']
-    attr_dict_highlrH['lr_H']= ['1.0']
+    if high_lr_H!= None and  low_lr_H!=None:
+        attr_dict_lowlrH['lr_H']= [low_lr_H]
+        attr_dict_highlrH['lr_H']= [high_lr_H]
     
-    print(f'attr_dict_highlrH : {attr_dict_highlrH}')
-    print(f'attr_dict_lowlrH : {attr_dict_lowlrH}')
+        print(f'attr_dict_highlrH : {attr_dict_highlrH}')
+        print(f'attr_dict_lowlrH : {attr_dict_lowlrH}')
+        
+    else:
+        print(f'highlrH : {high_lr_H} | lowlrH : {low_lr_H}')
         
     print('ploting heatmaps ... ')
     
