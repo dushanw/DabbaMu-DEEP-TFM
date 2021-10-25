b1438667ba4d712ba8a70edc1911aa433fadc0c2
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2021-10-25 14:50:41 -0400

    Added super-resolution eval metrics: PSNR, SSIM in luminance color channel

--- aim2/modules/eval_metrics.py
+++ aim2/modules/eval_metrics.py
@@ -2,6 +2,9 @@ import torch
 from torch.nn import functional as F
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 from ignite.metrics import SSIM as SSIM_IGNITE
+from ignite.metrics import PSNR
+
+import modules.pytorch_colors as colors
 
 
 def mse_distance(X_hat, X):
@@ -16,4 +19,22 @@ def ssim_ignite(X_hat, X, k= 11):
     metric = SSIM_IGNITE(data_range = 1.0, kernel_size= (k,k))
     metric.update((X_hat, X))
     return metric.compute().item()
+
+
+def get_y_channel(output):
+    y_pred, y = output
+    y_pred = colors.rgb_to_ycbcr(y_pred)[:,0,:,:] # select y-channel
+    y = colors.rgb_to_ycbcr(y)[:,0,:,:] # select y-channel
+    return y_pred, y
+
+def psnr_luminance(X_hat, X):
+    metric = PSNR(data_range=219, output_transform=get_y_channel) #(data_range= 235- 16= 219) ->  https://scikit-image.org/docs/dev/api/skimage.color.html#rgb2ycbcr 
+    metric.update((X_hat, X))
+    return metric.compute().item()
+    
+def ssim_luminance(X_hat, X, k= 11):
+    metric = SSIM_IGNITE(data_range = 219, kernel_size= (k,k), output_transform=get_y_channel) #(data_range= 235- 16= 219) ->  https://scikit-image.org/docs/dev/api/skimage.color.html#rgb2ycbcr 
+    metric.update((X_hat, X))
+    return metric.compute().item()
+    
     
