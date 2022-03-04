379d549edcd9aeb0aa4c0dd7e030ec8e943c2f48
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2022-03-04 00:03:30 -0500

    Added minor changes: flexibility interval for filtering

--- aim2/modules/summarizing_utils/common_filter_results_methods.py
+++ aim2/modules/summarizing_utils/common_filter_results_methods.py
@@ -38,7 +38,7 @@ def find_last_converged_result(img_dir, metric_name='SSIM', metric_type= 'score'
 
 
 
-def find_best_result(img_dir, metric_name='SSIM', metric_type= 'score'): #metric_type= loss/ score
+def find_best_result(img_dir, metric_name='SSIM', metric_type= 'score', flexibility_interval= 0.005): #metric_type= loss/ score
     img_list = sorted(glob.glob(f"{img_dir}/*.jpg"),key= sort_name_by_epoch, reverse=True)
     min_loss=1000
     final_img_dir= None
@@ -66,10 +66,10 @@ def find_best_result(img_dir, metric_name='SSIM', metric_type= 'score'): #metric
         if is_results_okay and metric!='nan':
             #loss= float(loss)
             if metric_type== 'loss':
-                if metric<min_metric+0.005:
+                if metric<min_metric+flexibility_interval:
                     return img_dir
             elif metric_type== 'score':
-                if metric>max_metric-0.005:
+                if metric>max_metric-flexibility_interval:
                     return img_dir
                 
             
@@ -77,21 +77,24 @@ def find_best_result(img_dir, metric_name='SSIM', metric_type= 'score'): #metric
     return None
 
 
-def get_img_list(img_dir = 'figs/mnistv6', mode='L1Loss', loss_threshold=0.05):
+def get_img_list(img_dir = 'figs/mnistv6', mode='L1Loss', loss_threshold=0.05, last_converged= False):
     exp_list = sorted(glob.glob(f'{img_dir}/*@*'))
+    
+    if last_converged:flexibility_interval= 10000 # then it will go until end even the metric is 10000 away from achieved optimal
+    else:flexibility_interval= 0.005 # it gives most last optimal result which have +-0.005 flexibility on the metric
     
     img_dirs=[]
     for idx in range(len(exp_list)):
         #if idx>102:break
         exp_dir = exp_list[idx]
-            
-        if mode=='last_converged_correct':img_dir = find_last_converged_correct_result(exp_dir, loss_threshold)
-        elif mode=='last_converged_MSE':img_dir = find_last_converged_result(exp_dir, metric_name='MSE', metric_type= 'loss')
-        elif mode=='L1Loss':img_dir = find_best_result(exp_dir, metric_name='L1Loss', metric_type= 'loss')
-        elif mode=='MSE':img_dir = find_best_result(exp_dir, metric_name='MSE', metric_type= 'loss')
-        elif mode=='SSIM':img_dir = find_best_result(exp_dir, metric_name='SSIM', metric_type= 'score')
-        elif mode=='SSIM5':img_dir = find_best_result(exp_dir, metric_name='SSIM5', metric_type= 'score')
-        elif mode=='SSIM11':img_dir = find_best_result(exp_dir, metric_name='SSIM11', metric_type= 'score')
+        
+        if mode=='L1Loss' or mode=='L1':
+            try:img_dir = find_best_result(exp_dir, metric_name='L1Loss', metric_type= 'loss', flexibility_interval= flexibility_interval)
+            except:img_dir = find_best_result(exp_dir, metric_name='L1', metric_type= 'loss', flexibility_interval= flexibility_interval)
+        elif mode=='MSE':img_dir = find_best_result(exp_dir, metric_name='MSE', metric_type= 'loss', flexibility_interval= flexibility_interval)
+        elif mode=='SSIM':img_dir = find_best_result(exp_dir, metric_name='SSIM', metric_type= 'score', flexibility_interval= flexibility_interval)
+        elif mode=='SSIM5':img_dir = find_best_result(exp_dir, metric_name='SSIM5', metric_type= 'score', flexibility_interval= flexibility_interval)
+        elif mode=='SSIM11':img_dir = find_best_result(exp_dir, metric_name='SSIM11', metric_type= 'score', flexibility_interval= flexibility_interval)
         
         if idx%100==0:
             print(f'{idx+1}/{len(exp_list)} : {img_dir}')
