269f93c27a3cfe856420269f6cfe795b02d9a97b
udithhaputhanthri <udithhaputhanthri.official@gmail.com>
2022-04-17 16:50:45 -0400

    Added deepTFM psfs

--- aim2/modules/psfs.py
+++ aim2/modules/psfs.py
@@ -1,6 +1,7 @@
 import scipy.io
 import os
 import torch
+import numpy as np
 
 def confocal_w1_exPSF(psf_dir= '../psfs'):
     exPSF = scipy.io.loadmat(f'{psf_dir}/psf_640.mat')['psf_640']
@@ -9,3 +10,7 @@ def confocal_w1_exPSF(psf_dir= '../psfs'):
 def confocal_w1_emPSF(psf_dir= '../psfs'):
     emPSF = scipy.io.loadmat(f'{psf_dir}/psf_700.mat')['psf_700']
     return torch.from_numpy(emPSF).float()
+
+def load_psf_from_npy(psf_dir):
+    psf = torch.from_numpy(np.load(psf_dir)).float()
+    return psf
